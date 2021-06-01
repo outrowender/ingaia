31da7b503b8b3615f7456ae1c16a234cbb89301a
Wender <outrowender@gmail.com>
2021-06-01 00:07:49 -0300

    tests c# adicionados

---
+++ back/v2/metric-api/tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Xunit;
+
+namespace metric_api.tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            //Assert
+            Assert.True(true);
+        }
+    }
+}
