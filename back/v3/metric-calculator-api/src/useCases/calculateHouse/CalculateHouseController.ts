3e3d7c677906488b2061b3b87e1431ed9592d4bd
Wender <outrowender@gmail.com>
2021-06-04 16:53:14 -0300

    house controller melhorado

--- back/v3/metric-calculator-api/src/useCases/calculateHouse/CalculateHouseController.ts
+++ back/v3/metric-calculator-api/src/useCases/calculateHouse/CalculateHouseController.ts
@@ -3,17 +3,30 @@ import { CalculateHouseUseCase } from "./CalculateHouseUseCase";
 
 export class CalculateHouseController {
     constructor(private useCase: CalculateHouseUseCase) { }
+
     async handle(request: Request, response: Response): Promise<Response> {
+
+        const valid = this.validate(request.body)
+        if (!valid) return response.status(422).json({ message: 'Unexpected Error' })
 
         const { size } = request.body
 
         try {
             const result = await this.useCase.execute({ size })
             return response.status(200).send(result)
+
         } catch (error) {
-            response.status(400).json({
+            return response.status(400).json({
                 message: error.message || 'Unexpected Error'
             })
         }
+    }
+
+    private validate(obj: object) {
+        if (!obj['size']) return false //check value
+        if (isNaN(obj['size'])) return false //check type
+        if (obj['size'] < 10 || obj['size'] > 10000) return false //check range
+
+        return true
     }
 }
---
+++ back/v3/metric-calculator-api/src/useCases/calculateHouse/spec/CalculateHouseUseCase.spec.ts
@@ -0,0 +1,52 @@
+import { calculateHouseUseCase, houseRepository } from ".."
+
+describe('CalculateHouseUseCase', () => {
+    it('should return calculated value when input is ok', async () => {
+        //arrange
+        const request = { size: 10 }
+        jest.spyOn(houseRepository, 'getSquareMeterValue').mockResolvedValue({ meters: 1, value: 10 })
+
+        //act
+        const val = await calculateHouseUseCase.execute(request)
+
+        //assert
+        expect(val).toEqual({
+            result: 100,
+            size: 10,
+            meterValue: 10
+        })
+    })
+
+    it('should throw an error when meter value is invalid (too low)', async () => {
+        //arrange
+        const request = { size: 10 }
+        jest.spyOn(houseRepository, 'getSquareMeterValue').mockResolvedValue({ meters: 1, value: 0 })
+
+        //act & assert
+        await expect(async () => {
+            await calculateHouseUseCase.execute(request)
+        }).rejects.toThrow('Error calculating values')
+    })
+
+    it('should throw an error when size is invalid (too low)', async () => {
+        //arrange
+        const request = { size: 0 }
+        jest.spyOn(houseRepository, 'getSquareMeterValue').mockResolvedValue({ meters: 1, value: 10 })
+
+        //act & assert
+        await expect(async () => {
+            await calculateHouseUseCase.execute(request)
+        }).rejects.toThrow('Error calculating values')
+    })
+
+    it('should throw an error when size is invalid (too high)', async () => {
+        //arrange
+        const request = { size: 10001 }
+        jest.spyOn(houseRepository, 'getSquareMeterValue').mockResolvedValue({ meters: 1, value: 10 })
+
+        //act & assert
+        await expect(async () => {
+            await calculateHouseUseCase.execute(request)
+        }).rejects.toThrow('Error calculating values')
+    })
+})
