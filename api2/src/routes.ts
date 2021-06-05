3f5d854a935b7ad4f25a8209d3cdec0117553df0
Wender <outrowender@gmail.com>
2021-06-05 18:26:03 -0300

    swagger adicionado na api2

--- api2/src/routes.ts
+++ api2/src/routes.ts
@@ -2,12 +2,17 @@ import { Router } from "express";
 import { adaptRoute } from "./shared/adapters/ExpressRouteAdapter";
 import { calculateHouseController } from "./useCases/calculateHouse";
 
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from '../docs/swagger.json';
+
 const router = Router()
 
-router.get('/', (req, res) => {
-    return res.status(200).send('POST {size: value} to use calculator')
-})
+//documentation routes
+router.use('/swagger', swaggerUi.serve);
+router.get('/swagger', swaggerUi.setup(swaggerDocument));
 
+//api routes
+router.get('/', (req, res) => (res.redirect('/swagger')))
 router.post('/', adaptRoute(calculateHouseController))
 
 export { router }
